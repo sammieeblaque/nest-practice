80a7ef827e7992aa5cdccdf89577d957899bb68f
Sammiee <88675348+sammieeblaque@users.noreply.github.com>
2024-11-21 12:04:30 +0100

    testing with products stuff

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  await app.listen(8080);
+
+  app.enableCors({
+    origin: '*',
+  });
 }
 bootstrap();
---
+++ src/products/products.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Injectable()
+export class ProductsService {
+  create(createProductDto: CreateProductDto) {
+    return {
+      createProductDto,
+    };
+  }
+
+  findAll() {
+    return `This action returns all products`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return {
+      id,
+      updateProductDto,
+    };
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
